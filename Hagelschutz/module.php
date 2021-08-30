15c45bf5daa38ab7607ef9a6f584c0b7060bc8ed
DonCri <36209368+DonCri@users.noreply.github.com>
2021-08-30 20:00:07 +0200

    Add files via upload

--- Hagelschutz/module.php
+++ Hagelschutz/module.php
@@ -19,12 +19,12 @@ class Hagelschutz extends IPSModule
 		if(!IPS_VariableProfileExists("HailState")) {
 			IPS_CreateVariableProfile("HailState", 0); // 0 = Boolean, 1 = Integer, 2 = Float, 3 = String
 			IPS_SetVariableProfileAssociation("HailState", true, $this->Translate("HailStateOn"), "", 0x00FF00); // String_WertName kann mit $$this->translate("ID") in locale.json übersetzten
-			IPS_SetVariableProfileAssociation("HailState", false, $this->Translate("HailStateOff"), ""); // String_WertName kann mit $$this->translate("ID") in locale.json übersetzten
+			IPS_SetVariableProfileAssociation("HailState", false, $this->Translate("HailStateOff"), "", -1); // String_WertName kann mit $$this->translate("ID") in locale.json übersetzten
 		}
 		
 		if(!IPS_VariableProfileExists("HailWarning")) { 
 			IPS_CreateVariableProfile("HailWarning", 1); // 0 = Boolean, 1 = Integer, 2 = Float, 3 = String
-			IPS_SetVariableProfileAssociation("HailWarning", 0, $this->Translate("NoHail"), ""); // String_WertName kann mit $$this->translate("ID") in locale.json übersetzten 
+			IPS_SetVariableProfileAssociation("HailWarning", 0, $this->Translate("NoHail"), "", -1); // String_WertName kann mit $$this->translate("ID") in locale.json übersetzten 
 			IPS_SetVariableProfileAssociation("HailWarning", 1, $this->Translate("Hail"), "", 0xFF0000); // String_WertName kann mit $$this->translate("ID") in locale.json übersetzten
 			IPS_SetVariableProfileAssociation("HailWarning", 2, $this->Translate("TestHail"), "", 0x00FF00); // String_WertName kann mit $$this->translate("ID") in locale.json übersetzten
 		}
